3fd3dd93db7a7a73908855a5e0f5396c53352d8b
Sathvika2003 <116025865+Sathvika2003@users.noreply.github.com>
2023-05-28 21:21:45 +0530

    Add files via upload
    Task-2
    Number Guessing Game

--- java tasks/game.java
+++ java tasks/game.java
@@ -1,1 +1,33 @@
-
+package project;
+import java.util.*;
+public class task2
+{
+    public static void main(String args[])
+    {
+        Random r=new Random();
+        int r1=r.nextInt(100);
+        int score=100,chance=10;
+       Scanner sc = new Scanner(System.in);
+        for(int i=0;i<chance;i++)
+        {
+            System.out.println("Enter your number ");
+            int num=sc.nextInt();
+            if(r1==num)
+            {
+                System.out.println("Hurray! your number is Matched with Generated number ");
+                System.out.print("Your current guessing score is "+score+"%");
+                break;
+            }
+            else if(num<r1)
+            {
+                score=score-10;
+                System.out.println("Sorry Your number is less than Generated Number! PLEASE TRY AGAIN");
+            }
+            else if(num>r1)
+            {
+                score=score-10;
+                System.out.println("sorry Your number is More than Generated Number! PLEASE TRY AGAIN");
+            }
+        }
+    }
+}
